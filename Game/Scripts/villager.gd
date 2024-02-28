ae8b55388b7578e965519a28c4602e7aec122860
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2024-02-27 21:18:16 -0500

    Finite State Machine started
    Got the basics for a finite state machine down and a prototype that can be tweeked to be a villager for testing the state machine

---
+++ Game/Scripts/villager.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+class_name Villager
+
+@export var max_speed = 60.0
+@export var acceleration = 50
+
+
+@onready var fsm = $FiniteStateMachine as FiniteStateMachine
+@onready var villager_wander_state = $FiniteStateMachine/VillagerWanderState as VillagerWanderState
+@onready var timer = $Timer
+@onready var villager_idle_state = $FiniteStateMachine/VillagerIdleState as VillagerIdleState
+
+func _ready():
+	fsm.state = villager_idle_state
+	randomize()
+	
+func choose(arr):
+	arr.shuffle()
+	return arr.front()
+	
+
+func _on_timer_timeout():
+	timer.wait_time = choose([4,5,6])
+	fsm.change_state(choose([villager_wander_state, villager_idle_state]))
