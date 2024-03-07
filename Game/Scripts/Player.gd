8096b33c95330c27d4f496f667368581ab82e776
snelso24 <snelso24@nd.edu>
2024-03-07 16:00:35 -0500

    Commit 1
    Commit

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -750.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
