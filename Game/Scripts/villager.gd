27de6daeeb0ed7c235b797a861eadbe228c135fa
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2024-03-07 12:56:27 -0500

    more fleshed out villager behavior
    added 3 types of villagers that are easy to slightly modify/copy and paste to flesh out towns. can use a similar model of finite states to work on enemies in the platform levels.

--- Game/Scripts/villager.gd
+++ Game/Scripts/villager.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 class_name Villager
 
-@export var max_speed = 60.0
-@export var acceleration = 50
+#@export var max_speed = 60.0
+#@export var acceleration = 50
 
 
 @onready var fsm = $FiniteStateMachine as FiniteStateMachine
-@onready var villager_wander_state = $FiniteStateMachine/VillagerWanderState as VillagerWanderState
+#@onready var villager_wander_state = $FiniteStateMachine/VillagerWanderState as VillagerWanderState
 @onready var timer = $Timer
 @onready var villager_idle_state = $FiniteStateMachine/VillagerIdleState as VillagerIdleState
 
@@ -20,5 +20,5 @@ func choose(arr):
 	
 
 func _on_timer_timeout():
-	timer.wait_time = choose([4,5,6])
-	fsm.change_state(choose([villager_wander_state, villager_idle_state]))
+	pass
+	
