ae83687b05b98f185460118fd1f139eaa5ac6a30
MangoGrove <113562356+MangoGrove@users.noreply.github.com>
2024-04-03 03:02:55 -0400

    Scene Transition Work
    Scene transitions

--- Game/Scripts/ChangeScene.gd
+++ Game/Scripts/ChangeScene.gd
@@ -4,14 +4,15 @@ var  entered = false
 
 func _on_body_entered(body: PhysicsBody2D):
 	entered = true
-	pass # Replace with function body.
+	# Replace with function body.
 
 
-func _on_body_exited(body):
+
+func _on_body_exited(body: PhysicsBody2D):
 	entered = false
-	pass # Replace with function body.
+ # Replace with function body.
 	
-func  _sceneChange(delta):
+func  _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("ui_accept"):
 			get_tree().change_scene_to_file("res://Scenes(Prefabs)/New Level.tscn")
