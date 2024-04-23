a952ca823bc35daa1ff25097bf97c41c63221bf9
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2024-04-23 16:41:03 -0400

    Retry on main
    Trying to add a new folder called New_Game that we can use instead of Game. Will eventually delete Game and make New_Game just Game, but keeping both folders for now incase there are any files that need to be copied

---
+++ New_Game/Scenes(Prefabs)/village_player_detect.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var fsm = $"../FiniteStateMachine" as FiniteStateMachine
+@onready var idle_state = $"../FiniteStateMachine/VillagerIdleState" as VillagerIdleState
+@onready var detected_state = $"../FiniteStateMachine/PlayerDetectedState"
+# This function is called when a physics body enters the detection area
+
+func _on_body_entered(body):
+	if body.name == "Player":
+	# Perform actions when the player enters the area
+		queue_free()
+		fsm.change_state(detected_state)
+
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		print("Player left area")
+		await get_tree().create_timer(.5).timeout
+		# testing out delaying a function, makes it so villager doesnt immediatly swap to idle
+		fsm.change_state(idle_state)
+	pass # Replace with function body.
---
+++ New_Game/ui/dialog_player.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+
+@export_file("*json") var scene_text_file
+
+var scene_text = {}
+var selected_text = []
+var in_progress = false
+
+@onready var background = $Background
+@onready var text_label = $TextLabel
+
+func _ready():
+	background.visible = false
+	scene_text = load_scene_text()
+	SignalBus.display_dialog.connect(on_display_dialog)
+	
+func load_scene_text():
+	var file = FileAccess.open(scene_text_file, FileAccess.READ)
+	if file.file_exists(scene_text_file):
+		file.open(scene_text_file, FileAccess.READ)
+		return JSON.parse_string(file.get_as_text())
+		
+		
+func show_text():
+	text_label.text = selected_text.pop_front()
+	
+func next_line():
+	if selected_text.size() > 0:
+		show_text()
+	else:
+		finish()
+
+func finish():
+	text_label.text = ""
+	background.visible = false
+	in_progress = false
+	#get_tree().paused = false
+
+func on_display_dialog(text_key):
+	if in_progress:
+		next_line()
+		
+	else:
+		#get_tree().paused = true
+		background.visible = true
+		in_progress = true
+		selected_text = scene_text[text_key].duplicate()
+		show_text()
+			
+
+
+
